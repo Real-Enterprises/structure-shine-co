539c8817c2799c79ae35297b35a9924f9df6efa7
AfnanHussain10 <afnan.h5050@gmail.com>
2026-04-22 00:28:08 +0500

    Add Google Tag Manager integration for enhanced tracking

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -60,6 +60,13 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        <Script id="gtm" strategy="afterInteractive">
+          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-NQZPQPPK');`}
+        </Script>
         <Script
           src="https://www.googletagmanager.com/gtag/js?id=AW-17975159401"
           strategy="afterInteractive"
@@ -74,6 +81,14 @@ export default function RootLayout({
         </Script>
       </head>
       <body id="site-top" className="font-sans antialiased">
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-NQZPQPPK"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
         {children}
         <Analytics />
         <SpeedInsights />
